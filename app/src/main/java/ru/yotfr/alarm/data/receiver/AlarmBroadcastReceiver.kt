8e2f604f16dc6c8874b39c0330d61ef2308659a2
Yotfr <yotfr73@gmail.com>
2023-09-11 18:02:35 +0400

    repeating alarm logic

--- app/src/main/java/ru/yotfr/alarm/data/receiver/AlarmBroadcastReceiver.kt
+++ app/src/main/java/ru/yotfr/alarm/data/receiver/AlarmBroadcastReceiver.kt
@@ -3,7 +3,6 @@ package ru.yotfr.alarm.data.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import ru.yotfr.alarm.data.service.AlarmService
 
 class AlarmBroadcastReceiver : BroadcastReceiver() {
@@ -11,7 +10,6 @@ class AlarmBroadcastReceiver : BroadcastReceiver() {
         context?.let {
             intent?.let { intent ->
                 val id = intent.getLongExtra("ID",0L)
-                Log.d("TEST","BROADCAST GET ID $id")
                 id.takeIf { it != 0L }?.let { id ->
                     val serviceIntent = Intent(context, AlarmService::class.java)
                         .putExtra("ID", id)
--- app/src/main/java/ru/yotfr/alarm/data/service/AlarmService.kt
+++ app/src/main/java/ru/yotfr/alarm/data/service/AlarmService.kt
@@ -4,7 +4,6 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.net.toUri
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,11 +13,11 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import ru.yotfr.alarm.MainActivity
 import ru.yotfr.alarm.R
+import ru.yotfr.alarm.domain.model.AlarmModel
 import ru.yotfr.alarm.domain.repository.AlarmRepository
 import ru.yotfr.alarm.domain.repository.AlarmScheduler
-import ru.yotfr.alarm.domain.model.WeekDays
+import ru.yotfr.alarm.ui.createalarm.mapper.toDayOfWeek
 import ru.yotfr.alarm.ui.navigation.MY_URI_RING
-import java.time.DayOfWeek
 import java.time.LocalDate
 import javax.inject.Inject
 
@@ -36,20 +35,15 @@ class AlarmService : Service() {
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d("TEST","Service Started")
         intent?.let {
             if (intent.getBooleanExtra("CANCEL", false)) {
                 stopSelf()
             }
-            Log.d("TEST","Service Started ${intent.getLongExtra("ID",124125)}")
             intent.getLongExtra("ID",0L).takeIf { it != 0L }?.let {
-
-                Log.d("TEST","Alrm Id $it")
-
-//                scope.launch {
-//                    val alarm = alarmRepository.getAlarmById(it)
-//                    Log.d("TEST","Alarm $alarm")
-//                }
+                scope.launch {
+                    val alarm = alarmRepository.getAlarmById(it)
+                    scheduleNewAlarm(alarm)
+                }
                 val notification = NotificationCompat.Builder(this, "NOTIFICATION_CHANNEL_SERVICE")
                     .setContentTitle("Alarm")
                     .setContentText("Ringing...")
@@ -80,6 +74,20 @@ class AlarmService : Service() {
             putExtra("CANCEL", true)
         }
         return PendingIntent.getService(this, 0, intentSelf, PendingIntent.FLAG_IMMUTABLE)
+    }
+
+    private suspend fun scheduleNewAlarm(alarmModel: AlarmModel) {
+        if (alarmModel.activeDays.isEmpty()) return
+        var nowDate = LocalDate.now()
+        do {
+            nowDate = nowDate.plusDays(1)
+        } while (!alarmModel.activeDays.map { it.toDayOfWeek() }.contains(nowDate.dayOfWeek))
+        val updatedAlarmModel = alarmModel.copy(
+            triggerTime = alarmModel.triggerTime.toLocalTime().atDate(nowDate)
+        )
+        alarmRepository.updateAlarm(updatedAlarmModel)
+        alarmScheduler.cancelAlarm(updatedAlarmModel.id)
+        alarmScheduler.scheduleAlarm(updatedAlarmModel.triggerTime, updatedAlarmModel.id)
     }
 
 }
